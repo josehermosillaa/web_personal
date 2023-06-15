10c8738d2a2e4f46e58b23f228b4927d5f310437
josehermosilla <jose.hermosillaa@usach.cl>
2023-06-15 00:07:47 -0400

    se configura que podamos ver las imagenes en modo debug, que sirva ficheros media

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from core import views
 
+from django.conf import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
@@ -26,3 +28,9 @@ urlpatterns = [
     path("portfolio/", views.portfolio, name="portfolio"),
 
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
