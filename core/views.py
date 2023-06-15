134970497da2e26f16a387a38be12c7217115c7c
josehermosilla <jose.hermosillaa@usach.cl>
2023-06-14 23:00:14 -0400

    primer commit maqueta lista de pagina personal con formato front

---
+++ core/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse
+
+
+html_base = """
+<h1>MI WEB PERSONAL</h1>
+"<h1>MI WEB PERSONAL</h1><h2>Portada</h2>"
+"""
+
+
+def home(request):
+    return render(request,"core/home.html")
+
+def about(request):
+    return render(request,"core/about.html")
+def contact(request):
+    return render(request,"core/contact.html")
+
+def portfolio(request):
+    return render(request,"core/portfolio.html")
+    
