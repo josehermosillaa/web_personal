773854078a8c9a01bbdb7af512d55227c8a01bd7
josehermosilla <jose.hermosillaa@usach.cl>
2023-06-15 00:04:38 -0400

    se crea mediaroot y medias, ademas se añade el campo uploadto media-projects [ [

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     description=models.TextField(verbose_name="Descripción")
-    image = models.ImageField(verbose_name="Imagen")
+    image = models.ImageField(verbose_name="Imagen",upload_to="projects")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualización")
 
